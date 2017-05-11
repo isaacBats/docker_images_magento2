0fa0cdc4c505fbe69b73a9dc4db00bd6976b644b
Iván Miranda <ivan.miranda@sincco.com>
2017-05-11 16:32:23 -0500

    Error en m2install

--- 2.1.x/Dev/Web/bin/m2install.sh
+++ 2.1.x/Dev/Web/bin/m2install.sh
@@ -8,7 +8,7 @@ then
 else
 	echo -e "${COLOR}Obteniendo Magento2..."
 	cd /var/www/html
-	composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition="$MAGENTO_VERSION" ./
+	composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition="MAGENTO_VERSION" ./
 	composer config secure-http false
 	composer config minimum-stability beta
 	composer install
