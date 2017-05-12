51193c19d2a0a91004b6847b1cea10d332d35523
Iván Miranda <ivan.miranda@sincco.com>
2017-05-12 10:28:39 -0500

    Correcion m2install

--- 2.1.x/Dev/Web/bin/m2install.sh
+++ 2.1.x/Dev/Web/bin/m2install.sh
@@ -15,7 +15,7 @@ else
 	echo -e "${COLOR}Instalando Magento2..."
 	php bin/magento setup:install --admin-firstname=pengo --admin-lastname=admin --admin-email=$MAGENTO_ADMIN_EMAIL --admin-user=$MAGENTO_ADMIN_USER --admin-password=$MAGENTO_ADMIN_PASSWORD --db-host=$MAGENTO_DB_HOST --db-name=$MAGENTO_DB_NAME --db-user=$MAGENTO_DB_USERNAME --db-password=$MAGENTO_DB_PASSWORD --currency=$MAGENTO_CURRENCY --timezone=$MAGENTO_TIMEZONE --language=$MAGENTO_LANGUAGE --base-url=$MAGENTO_BASE_URL --backend-frontname=$MAGENTO_BACKEND_FRONTNAME
 	echo -e "${COLOR}Instalando Humboldt..."
-	composer config repositories.humboldt composer http://devops.pengostores.mx:5000
+	composer config repositories.humboldt composer http://staging.pengostores.mx:5000
 	composer require pengo/humboldt
 	cp /root/.composer/auth.json ./auth.json
 	chown www-data:www-data /var/www/html -Rf
