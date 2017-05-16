3cc1ed55798f98f9ab88373181772be73716af2f
Huetzin <huetzin@pengostores.com>
2017-05-16 12:59:27 -0500

    webdev image

--- 2.1.x/Dev/Web/bin/m2install.sh
+++ 2.1.x/Dev/Web/bin/m2install.sh
@@ -18,11 +18,12 @@ else
 	composer config repositories.humboldt composer http://staging.pengostores.mx:5000
 	composer require pengo/humboldt
 	cp /root/.composer/auth.json ./auth.json
-	chown www-data:www-data /var/www/html -Rf
+	chown root:www-data /var/www/html -Rf
 	echo -e "${COLOR}Activando Humboldt..."
 	php bin/magento module:enable Pengo_Humboldt
 	php bin/magento setup:upgrade
-	chown www-data:www-data /var/www/html -Rf
+	php bin/magento setup:di:compile
 	find . -type d -exec chmod 770 {} \; &&  find . -type f -exec chmod 660 {} \; &&  chmod u+x bin/magento
+	chown root:www-data /var/www/html -Rf
 	echo -e "${COLOR}Ahora puedes accesar desde $MAGENTO_BASE_URL $MAGENTO_BACKEND_FRONTNAME${NC}"
 fi
