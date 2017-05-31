664e6fd89bddaa40e1ec83177ac6426cc01e66c2
Iván Miranda <ivan.miranda@sincco.com>
2017-05-31 13:29:27 -0500

    Soporte Humboldt conSATIS y manejo de versiones

--- 2.1.x/Dev/Web/bin/m2install.sh
+++ 2.1.x/Dev/Web/bin/m2install.sh
@@ -4,13 +4,15 @@ NC='\033[0m'
 file="/var/www/html/bin/magento"
 if [ -f "$file" ]
 then
+	chown root:www-data /var/www/html -Rf
 	echo -e "${COLOR}Magento ya está instalado."
+	echo -e "${COLOR}Ahora puedes accesar desde $MAGENTO_BASE_URL $MAGENTO_BACKEND_FRONTNAME${NC}"
 else
 	echo -e "${COLOR}Obteniendo Magento2..."
 	cd /var/www/html
 	composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition="MAGENTO_VERSION" ./
 	composer config secure-http false
-	composer config minimum-stability beta
+	composer config minimum-stability alpha
 	composer install
 	echo -e "${COLOR}Instalando Magento2..."
 	php bin/magento setup:install --admin-firstname=pengo --admin-lastname=admin --admin-email=$MAGENTO_ADMIN_EMAIL --admin-user=$MAGENTO_ADMIN_USER --admin-password=$MAGENTO_ADMIN_PASSWORD --db-host=$MAGENTO_DB_HOST --db-name=$MAGENTO_DB_NAME --db-user=$MAGENTO_DB_USERNAME --db-password=$MAGENTO_DB_PASSWORD --currency=$MAGENTO_CURRENCY --timezone=$MAGENTO_TIMEZONE --language=$MAGENTO_LANGUAGE --base-url=$MAGENTO_BASE_URL --backend-frontname=$MAGENTO_BACKEND_FRONTNAME
@@ -18,13 +20,21 @@ else
 	composer config repositories.humboldt composer http://staging.pengostores.mx:5000
 	composer require pengo/humboldt
 	composer require pengo/backendtheme
-	cp /root/.composer/auth.json ./auth.json
+	#cp /root/.composer/auth.json ./auth.json
 	chown root:www-data /var/www/html -Rf
 	echo -e "${COLOR}Activando Humboldt..."
 	php bin/magento module:enable Pengo_Humboldt
 	php bin/magento setup:upgrade
-	php bin/magento setup:di:compile
+	#php bin/magento setup:di:compile
 	find . -type d -exec chmod 770 {} \; &&  find . -type f -exec chmod 660 {} \; &&  chmod u+x bin/magento
 	chown root:www-data /var/www/html -Rf
+	chown www-data:www-data /var/www/html/var -Rf
+	chmod 774 /var/www/html/var -Rf
+	#Soporte para ejecutar composer desde php
+	cp /root/composer_update* ./
+	chown root composer_update.sh
+	chmod u=rwx,go=xr composer_update.sh
+	chown root composer_update
+	chmod u=rwx,go=xr,+s composer_update
 	echo -e "${COLOR}Ahora puedes accesar desde $MAGENTO_BASE_URL $MAGENTO_BACKEND_FRONTNAME${NC}"
 fi
